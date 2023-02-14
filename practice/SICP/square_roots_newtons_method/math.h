3edd105c1fd79e3c8628ba713162aa56c75be358
William Rabbermann <105755092+willrabbermann@users.noreply.github.com>
2023-02-14 15:13:25 -0600

    Update math.h

--- practice/SICP/square_roots_newtons_method/math.h
+++ practice/SICP/square_roots_newtons_method/math.h
@@ -1,13 +1,12 @@
 #pragma once
 
-#define abs(a) ((a < 0) ? (-a) : (a)) 
 #define aprox(a, b, c) ((abs((a - b)) < c) ? (1) : (0))
 #define average(x, y) ((x + y) / 2)
 #define square(x) (x * x)
-
 #define improve_guess(guess, x) (average(guess, (x / guess)))
+
 // iterative preprocessor macros are not possible in C, unfortunately.
-//#define sqrt_iter_float(guess, x) (aprox(square(guess), x, 1e-6f) ? (guess) : (sqrt_iter_float(improve_guess(guess, x)), x))
+//#define sqrt_iter(guess, x) (aprox(square(guess), x, 1e-6f) ? (guess) : (sqrt_iter(improve_guess(guess, x)), x))
 
 long double sqrt_iter(long double guess, long double x, long double precision)
 {
