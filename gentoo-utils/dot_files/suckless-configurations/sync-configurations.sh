89d0dcee5db02c6ecfb7d1e57debe3ad5acbc798
willrabbermann <willrabbermann@gmail.com>
2023-04-20 19:32:17 -0500

    modified:   sync-configurations.sh

--- gentoo-utils/dot_files/suckless-configurations/sync-configurations.sh
+++ gentoo-utils/dot_files/suckless-configurations/sync-configurations.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-# `rsync` my suckless configs and remove the `.git/` directories then `git add` them for deltas.
+# rsync my suckless configs and remove the .git/ submodule directories then `git add` them for deltas.
 
-rsync -aHAXhvPS /mnt/nfs/block0/backup/important/dot_files/custom-files/gentoo/dwm-custom-configs/* /home/will/projects/github/utilities/gentoo-utils/dot_files/suckless-configurations --exclude=*.7z
+rsync --del -aHAXhvPS /mnt/nfs/block0/backup/important/dot_files/custom-files/gentoo/dwm-custom-configs/* /home/will/projects/github/utilities/gentoo-utils/dot_files/suckless-configurations --exclude=.git/ --exclude=*.7z --exclude="sync-configurations.sh"
 
 cd /home/will/projects/github/utilities/gentoo-utils/dot_files/suckless-configurations
-
-echo -e "\nRemoving */.git/ submodules from $(pwd)\n"
-rm */.git/ -rfv
 
 echo -e "\nAdding all new files to git...\n"
 git add -v *
