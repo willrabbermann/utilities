70329285eadb6cc528c666feca9e16ef3cdace7f
willrabbermann <willrabbermann@gmail.com>
2024-01-26 11:48:41 -0600

    gentoo.conf to control vfio-pci-override

--- gentoo-utils/dot_files/dracut/modules.d/20vfio-override/vfio-pci-override.sh
+++ gentoo-utils/dot_files/dracut/modules.d/20vfio-override/vfio-pci-override.sh
@@ -5,10 +5,12 @@ DEVICES=(
 		"0000:01:00.1 " # 3060 (audio)
 )
 
-for dev in ${DEVICES[@]}; do
-	echo "vfio-pci" > /sys/bus/pci/devices/$dev/driver_override
-done
+if [[ -n $(grep VIDEO_CARD_VFIO_PASSTHROUGH=1 /opt/gentoo.conf) ]]
+then
+	for dev in ${DEVICES[@]}; do
+		echo "vfio-pci" > /sys/bus/pci/devices/$dev/driver_override
+	done
 
-modprobe -i vfio-pci
-
+	modprobe -i vfio-pci
+fi
 
